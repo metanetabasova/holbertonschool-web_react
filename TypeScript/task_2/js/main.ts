f9430d6b0047de411f4b5563880aea77abb2025b
metanetabasova <metanetabasova314@gmail.com>
2026-08-15 09:54:21 +0000

    Add

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -45,7 +45,17 @@ export function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
 // Nümayə yoxlanışları:
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+console.log(executeWork(createEmployee(200)));  // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
